8b40a09a369b95c0e21e89f97b79369332e7f795
FalqahiAkbarAdzaki <dragontamer1925@gmail.com>
2024-10-22 10:58:04 +0700

    2

--- temperature/temperature.py
+++ temperature/temperature.py
@@ -1,1 +1,16 @@
+def konversisuhu(temperature, value):
+    if value == 'C':
+        temperaturesuhu = (temperature - 32) * 5/9
+        return temperaturesuhu, 'C'
+    else:
+        temperaturesuhu = (temperature * 9/5) * 32
+        return temperaturesuhu, 'F'
 
+celsius_temperature = 30
+temperaturesuhu, target_value = konversisuhu(celsius_temperature, 'F')
+print(f"{celsius_temperature}°C dikonversi ke fahrenheit adalah{temperaturesuhu}°{target_value}")
+
+fahrenhait_temperature = 86
+temperaturesuhu, target_value = konversisuhu(fahrenhait_temperature, 'C')
+print(f"{fahrenhait_temperature}°F dikonversi ke Celsius adalah
+      {temperaturesuhu}°{target_value}")
