39fc479325a7ff0e8dabbd2bd17017f0d444561a
FalqahiAkbarAdzaki <dragontamer1925@gmail.com>
2024-10-22 11:21:19 +0700

    3

--- CreateFunction_161/CreateFunction_161.py
+++ CreateFunction_161/CreateFunction_161.py
@@ -1,8 +1,8 @@
 import math #library untuk operasi matematika
 
-luas_lingkaran = lambda r: math.pi * r*r
+luas_lingkaran = lambda r: math.pi * r*r #lambda = berfungsi untuk membuat fungsi anonim
 
 #contoh penggunaannya
-jari_jari =7
-area = luas_lingkaran(jari_jari)
-print(f"Luas lingkaran dengan jari-jari {jari_jari} adalah {area:.2f}")
+jari_jari =7# untuk menentukan nilai jari-jari sebuah lingkaran
+area = luas_lingkaran(jari_jari) #area=variabel yang menyimpan hitungan luas lingkaran
+print(f"Luas lingkaran dengan jari-jari {jari_jari} adalah {area:.2f}")#mencetak hasil akhir dari hitungan
--- temperature/temperature.py
+++ temperature/temperature.py
@@ -1,16 +1,18 @@
-def konversisuhu(temperature, value):
-    if value == 'C':
+def konversisuhu(temperature, value): #mendefinisikan fungsi konversi suhu
+    if value == 'C':# mengecek apakah suhunya menggunakan satuan celsius
         temperaturesuhu = (temperature - 32) * 5/9
-        return temperaturesuhu, 'C'
-    else:
+        return temperaturesuhu, 'C'#mengembalikan fungsi ke suhu celsius
+    else:#mengalihkan perhitungan menggunakan fahrenheit jika tidak menggunakan celsius
         temperaturesuhu = (temperature * 9/5) * 32
-        return temperaturesuhu, 'F'
+        return temperaturesuhu, 'F'#menunjukkan hasil suhu fahrenheit
 
 celsius_temperature = 30
 temperaturesuhu, target_value = konversisuhu(celsius_temperature, 'F')
 print(f"{celsius_temperature}°C dikonversi ke fahrenheit adalah{temperaturesuhu}°{target_value}")
+#mengkonversi nilai suhu celsius menjadi fahrenheit
 
 fahrenhait_temperature = 86
 temperaturesuhu, target_value = konversisuhu(fahrenhait_temperature, 'C')
 print(f"{fahrenhait_temperature}°F dikonversi ke Celsius adalah
       {temperaturesuhu}°{target_value}")
+    #mengkonversi nilai suhu fahrenheit menjadi celsius
